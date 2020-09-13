95150ca9e82715ece07baf0c50712165b21dcd0e
Dmitriy Novikov <mr.ronuar@gmail.com>
2020-09-13 12:47:32 +0300

    Упрощает стили анимации блока с соц. иконками

--- source/js/modules/social.js
+++
@@ -1,9 +0,0 @@
-export default () => {
-  let socialBlock = document.querySelector(`.js-social-block`);
-  socialBlock.addEventListener(`mouseover`, function () {
-    socialBlock.classList.add(`social-block--active`);
-  });
-  socialBlock.addEventListener(`mouseleave`, function () {
-    socialBlock.classList.remove(`social-block--active`);
-  });
-};
