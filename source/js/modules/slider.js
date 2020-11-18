332ec99042f4b34f5c2c4a2386ae2eb59e5181d6
Dmitriy Novikov <mr.ronuar@gmail.com>
2020-11-18 21:52:44 +0300

    Устанавливает тему при переключении экранов истории

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,9 +1,21 @@
 import Swiper from "swiper";
 
+const Theme = {
+  PURPLE_LIGHT: `purple-light`,
+  PURPLE: `purple`,
+  BLUE_LIGHT: `blue-light`,
+  BLUE: `blue`
+};
+
+function setTheme(theme) {
+  document.body.dataset.theme = theme;
+}
+
 export default () => {
   let storySlider;
   let sliderContainer = document.getElementById(`story`);
   sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
+  setTheme(Theme.PURPLE_LIGHT);
 
   const setSlider = function () {
     if (((window.innerWidth / window.innerHeight) < 1) || window.innerWidth < 769) {
@@ -17,15 +29,22 @@ export default () => {
         },
         on: {
           slideChange: () => {
+            let theme = Theme.PURPLE_LIGHT;
             if (storySlider.activeIndex === 0 || storySlider.activeIndex === 1) {
               sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
+              theme = Theme.PURPLE_LIGHT;
             } else if (storySlider.activeIndex === 2 || storySlider.activeIndex === 3) {
               sliderContainer.style.backgroundImage = `url("img/slide2.jpg"), linear-gradient(180deg, rgba(45, 54, 179, 0) 0%, #2A34B0 16.85%)`;
+              theme = Theme.BLUE;
             } else if (storySlider.activeIndex === 4 || storySlider.activeIndex === 5) {
               sliderContainer.style.backgroundImage = `url("img/slide3.jpg"), linear-gradient(180deg, rgba(92, 138, 198, 0) 0%, #5183C4 16.85%)`;
+              theme = Theme.BLUE_LIGHT;
             } else if (storySlider.activeIndex === 6 || storySlider.activeIndex === 7) {
               sliderContainer.style.backgroundImage = `url("img/slide4.jpg"), linear-gradient(180deg, rgba(45, 39, 63, 0) 0%, #2F2A42 16.85%)`;
+              theme = Theme.PURPLE;
             }
+
+            setTheme(theme);
           },
           resize: () => {
             storySlider.update();
@@ -51,15 +70,22 @@ export default () => {
         },
         on: {
           slideChange: () => {
+            let theme = Theme.PURPLE_LIGHT;
             if (storySlider.activeIndex === 0) {
               sliderContainer.style.backgroundImage = `url("img/slide1.jpg")`;
+              theme = Theme.PURPLE_LIGHT;
             } else if (storySlider.activeIndex === 2) {
               sliderContainer.style.backgroundImage = `url("img/slide2.jpg")`;
+              theme = Theme.BLUE;
             } else if (storySlider.activeIndex === 4) {
               sliderContainer.style.backgroundImage = `url("img/slide3.jpg")`;
+              theme = Theme.BLUE_LIGHT;
             } else if (storySlider.activeIndex === 6) {
               sliderContainer.style.backgroundImage = `url("img/slide4.jpg")`;
+              theme = Theme.PURPLE;
             }
+
+            setTheme(theme);
           },
           resize: () => {
             storySlider.update();
